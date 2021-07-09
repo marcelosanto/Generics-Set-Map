44ffe8f645c32365cbb72ec5d390782e98c491f9
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-09 14:39:27 -0300

    solucao usando generics

---
+++ src/introgenerics/Program.java
@@ -0,0 +1,27 @@
+package introgenerics;
+
+import introgenerics.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        PrintService<Integer> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            System.out.print("Digite o valor #" + i + ": ");
+            int value = sc.nextInt();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+
+        sc.close();
+    }
+}
