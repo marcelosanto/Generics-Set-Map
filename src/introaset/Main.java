0ae92a7ed3fde033e9f210578912e32423449a6e
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-10 08:40:25 -0300

    usando Set para testar igualdades

--- src/introaset/Main.java
+++ src/introaset/Main.java
@@ -1,5 +1,7 @@
 package introaset;
 
+import introaset.entities.Product;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -7,19 +9,14 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Integer> a = new TreeSet<>(Arrays.asList(0,2,4,5,6,8,10));
-        Set<Integer> b = new TreeSet<>(Arrays.asList(5,6,7,8,9,10));
-//union
-        Set<Integer> c = new TreeSet<>(a);
-        c.addAll(b);
-        System.out.println(c);
-//intersection
-        Set<Integer> d = new TreeSet<>(a);
-        d.retainAll(b);
-        System.out.println(d);
-//difference
-        Set<Integer> e = new TreeSet<>(a);
-        e.removeAll(b);
-        System.out.println(e);
+        Set<Product> set = new HashSet<>();
+
+        set.add(new Product("TV", 900.0));
+        set.add(new Product("Notebook", 1200.0));
+        set.add(new Product("Tablet", 400.0));
+
+        Product prod = new Product("Notebook", 1200.0);
+
+        System.out.println(set.contains(prod));
     }
 }
