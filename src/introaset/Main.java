9d912d6ace9051f03aada71cc7457cf6f2e7ec4a
Marcelo Santos <rocha.mer21@gmail.com>
2021-07-10 08:45:54 -0300

    usando TreeSet

--- src/introaset/Main.java
+++ src/introaset/Main.java
@@ -9,14 +9,14 @@ import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
-        Set<Product> set = new HashSet<>();
+        Set<Product> set = new TreeSet<>();
 
         set.add(new Product("TV", 900.0));
         set.add(new Product("Notebook", 1200.0));
         set.add(new Product("Tablet", 400.0));
 
-        Product prod = new Product("Notebook", 1200.0);
-
-        System.out.println(set.contains(prod));
+       for (Product pd : set){
+           System.out.println(pd);
+       }
     }
 }
